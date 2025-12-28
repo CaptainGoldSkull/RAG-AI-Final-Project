8fef30c37ed937fd0f4d400df014c9227f75ea65
CaptainGoldSkull <oliver.halpin@gmail.com>
2025-12-28 03:21:23 +0000

    First attempts at clearing files for fresh install

--- helpers/scraper.py
+++ helpers/scraper.py
@@ -3,6 +3,8 @@ import feedparser
 import os
 import zipfile
 
+from bs4 import BeautifulSoup
+
 #response = requests.get("https://cheatsheetseries.owasp.org/index.html")
 
 #print(response.text)
@@ -12,26 +14,58 @@ linkToParse = "https://cheatsheetseries.owasp.org/News.xml"
 
 parsed = feedparser.parse(linkToParse)
 
+def deepRemove(Path):
+    # Recursive function that will go through a folder and remove all contents
+    if os.path.isdir(Path):
+        for fileName in os.listdir(Path):
+            filePath = os.path.join(Path, fileName)
+            if os.path.isdir(filePath):
+                # Maybe need to add wait for return here...
+                try:
+                    print("Attemtping to remove directory.. "+ filePath)
+                    os.rmdir(filePath)
+                except:
+                    print("Calling deepRemove on " + filePath)
+                    deepRemove(filePath)
+            elif os.path.isfile(filePath):
+                print("Removing file " + filePath)
+                os.remove(filePath)
+        try:
+            os.rmdir(filePath)
+        except:
+            print("Couldnt remove directory.. " + filePath)
 
 def freshDownload(): # Use this for a fresh install/Complete refesh of data
     # Clear out any files in cachedPages
-    # Re-Cache all the pages from download at:
-    # https://cheatsheetseries.owasp.org/bundle.zip
-    if len(os.listdir("cachedPages")) > 1:
-        print("I need to add a clear function in here")
-    
+    #if len(os.listdir("cachedPages")) >= 1:
+    for fileName in os.listdir("cachedPages"):
+        # TO DO:
+        # MAKE THIS WORK FIRST TIME.... Not all files get cleared
+        print(fileName)
+        filePath = os.path.join("cachedPages", fileName)
+        if os.path.isdir(filePath):
+            print("removign")
+            try:
+                os.rmdir(filePath)
+            except:
+                deepRemove(filePath)
+    # Cache all the pages from download at:
+    # https://cheatsheetseries.owasp.org/bundle.zip    
 
     requestedData = requests.get(owaspBaseLink)
 
+    # Store a temporary zip fil;e in /temp
     with open("temp/owaspDelete.zip",mode="wb") as tempZip:
         tempZip.write(requestedData.content)
 
+    # decompress into cachedPages/owasp
     with zipfile.ZipFile("temp/owaspDelete.zip", 'r') as zipRef:
         zipRef.extractall("cachedPages/owasp")
         
+    # Remove temp zip
     os.remove("temp/owaspDelete.zip")
 
-    
+
 def checkForUpdates():
     # Check the "last updated" compared to the files last modified for the page for each
     # Get this information from https://cheatsheetseries.owasp.org/News.xml
